dbbfcca6e8fed8da332610c221be790ce5ac36bc
Cody Benkoski <cob322@lehigh.edu>
2019-09-06 17:25:43 -0400

    Installation helper script

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+sudo cp php7-gdal.so /usr/lib/php/20151012
+cd /usr/lib/php/20151012
+sudo chown $USER /usr/lib/php/20151012/php7-gdal.so #modify $USER to whatever user your PHP installation runs under
