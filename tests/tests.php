5659f57310b2f8a0f53aed89f0124f85866285b3
Cody Benkoski <cob322@lehigh.edu>
2019-09-15 20:30:42 -0400

    Updated test file

--- tests/tests.php
+++ tests/tests.php
@@ -17,3 +17,9 @@ expects(extension_loaded('php7-gdal'),"extension not loaded successfully", "exte
 expects(function_exists('gdal_registered_drivers'), "gdal_registered_drivers() is not available.", "gdal_registered_drivers() is available");
 expects(function_exists('gdal_register_all_drivers'), "gdal_register_all_drivers() is not available", "gdal_register_all_drivers() is available.");
 expects(function_exists("gdal_info"), "gdal_info() is not available", "gdal_info is available.");
+
+expects(gettype(gdal_registered_drivers()) === "integer", "Invalid type returned for gdal_registered_drivers()", "Valid type returned for gdal_registered_drivers()");
+
+gdal_register_all_drivers(); //load drivers to check next test
+
+expects(gettype(gdal_info("b.tif")) === "string", "Invalid type returned for gdal_info()","Valid type returned for gdal_info()");
