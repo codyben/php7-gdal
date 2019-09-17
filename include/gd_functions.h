ce99f5961fcaad27fb2457c52f86f18ca298a3b2
Cody Benkoski <cob322@lehigh.edu>
2019-09-16 20:45:30 -0400

    Added stubs for new gdal functions

--- include/gd_functions.h
+++ include/gd_functions.h
@@ -2,8 +2,11 @@
 extern "C" {
 	//at this point, I'm more concerned with making sure everything links correctly and PHP behaves, hence all the void types
 	Php::Value gdal_info(Php::Parameters &params); //mirrors gdalinfo functionality
-	void gdal_translate(); //mirrors gdal_translate functionality
-	void gdal_warp(); //mirrors gdalwarp functionality
-	void gdal_register_all_drivers(); //load all available drivers
+	Php::Value gdal_translate(); //mirrors gdal_translate functionality
+	Php::Value gdal_warp(); //mirrors gdalwarp functionality
+	Php::Value gdal_register_all_drivers(); //load all available drivers & print number loaded
 	Php::Value gdal_registered_drivers(); //get numeric count of loaded drivers
+	Php::Value gdal_addo(); //build or rebuild an overview image
+	Php::Value gdal_rasterize(); //move vector geometry onto an image
+	Php::Value gdal_config(); //output information about the gdal installation
 }
--- src/gd_functions.cpp
+++ src/gd_functions.cpp
@@ -9,61 +9,77 @@ extern "C" {
 #include <phpcpp.h>
 extern "C" {
 
-Php::Value gdal_registered_drivers()
-{
-	Php::Value count = GDALGetDriverCount(); //returns the number of currently registered drivers (int)
-	return count;
-}
-
-Php::Value gdal_register_all_drivers()
-{
-	GDALAllRegister(); //not sure how to treat errors here, so its a void
-}
-
-Php::Value gdal_info(Php::Parameters &params) //todo
-{
-    if(gdal_registered_drivers() == 0){
-		Php::error << "No GDAL Drivers detected" << std::flush;
-		return false;
-    }
-    //Php::warning << params.size() << std::endl;
-    Php::Value inter = params[0];
- 	//Php::warning << "no fault4" << std::endl;
-    std::string f = inter;
-    char** papszArgv = (char**)calloc(1,sizeof(char*));
-   //Php::warning << "no fault1" << std::endl;
-
-    //Php::warning << "no fault" << std::endl;
-	papszArgv[0] = "-json\0";
-
-	GDALInfoOptions* rzecz = GDALInfoOptionsNew(papszArgv, NULL);
-
-	free(papszArgv);
-
-	GDALDatasetH ds = GDALOpenEx(f.c_str(), GDAL_OF_READONLY | GDAL_OF_RASTER | GDAL_OF_VERBOSE_ERROR | GDAL_OF_VECTOR, NULL, NULL, NULL);
-
-	if(ds == NULL){
-		Php::warning << "No file found for "+f << std::flush;
-		return false;
+	Php::Value gdal_registered_drivers()
+	{
+		Php::Value count = GDALGetDriverCount(); //returns the number of currently registered drivers (int)
+		return count;
 	}
 
-	char* vals = GDALInfo(ds, rzecz);
+	Php::Value gdal_register_all_drivers()
+	{
+		GDALAllRegister();
+		return gdal_registered_drivers(); //return number of registered drivers.
+	}
 
-	std::string returnValue(vals);
+	Php::Value gdal_info(Php::Parameters &params) //todo
+	{
+	    if(gdal_registered_drivers() == 0){
+			Php::error << "No GDAL Drivers detected" << std::flush;
+			return false;
+	    }
+	    //Php::warning << params.size() << std::endl;
+	    Php::Value inter = params[0];
+	 	//Php::warning << "no fault4" << std::endl;
+	    std::string f = inter;
+	    char** papszArgv = (char**)calloc(1,sizeof(char*));
+	   //Php::warning << "no fault1" << std::endl;
 
-	return returnValue; //this outputs JSON, which I'll ideally convert into a native PHP Array
+	    //Php::warning << "no fault" << std::endl;
+		papszArgv[0] = "-json\0";
 
-}
+		GDALInfoOptions* rzecz = GDALInfoOptionsNew(papszArgv, NULL);
 
-Php::Value gdal_translate(Php::Parameters &params) //todo
-{
-    Php::out << "example output" << std::endl;
-    return -1;
-}
+		free(papszArgv);
 
-Php::Value gdal_warp(Php::Parameters &params)
-{
-    Php::out << "example output" << std::endl;
-    return -1;
-}
+		GDALDatasetH ds = GDALOpenEx(f.c_str(), GDAL_OF_READONLY | GDAL_OF_RASTER | GDAL_OF_VERBOSE_ERROR | GDAL_OF_VECTOR, NULL, NULL, NULL);
+
+		if(ds == NULL){
+			Php::warning << "No file found for "+f << std::flush;
+			return false;
+		}
+
+		char* vals = GDALInfo(ds, rzecz);
+
+		std::string returnValue(vals);
+
+		return returnValue; //this outputs JSON, which I'll ideally convert into a native PHP Array
+
+	}
+
+	Php::Value gdal_translate(Php::Parameters &params) //todo
+	{
+	    Php::out << "example output" << std::endl;
+	    return -1;
+	}
+
+	Php::Value gdal_warp(Php::Parameters &params) //todo
+	{
+	    Php::out << "example output" << std::endl;
+	    return -1;
+	}
+
+	Php::Value gdal_rasterize(Php::Parameters &params) //todo
+	{
+		return -1;
+	}
+
+	Php::Value gdal_addo(Php::Parameters &params) //todo
+	{
+		return -1;
+	}
+
+	Php::Value gdal_config(Php::Parameters &params) //todo
+	{
+		return -1;
+	}
 }
